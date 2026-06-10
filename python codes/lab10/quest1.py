b36970f647659ec62d14947ea5ae30109228eecc
vini_mafra <vinimafra1312@gmail.com>
2026-06-10 13:16:02 -0300

    atvdd de for

---
+++ python codes/lab10/quest1.py
@@ -0,0 +1,7 @@
+soma = 0
+
+for i in range(1, 11):
+    valor = int(input(f"Digite o valor {i}: "))
+    soma = soma + valor
+
+print(f"Soma total: {soma}")
---
+++ python codes/lab10/quest2.py
@@ -0,0 +1,21 @@
+#a)
+for i in range(0,5,1):
+    print(i)
+    
+
+
+
+#b)
+i = 1      # inicio  
+fim = 10   # fim
+while i < fim:
+    print(i)
+    i += 2 # pulo
+
+
+#c)
+i = 10     # inicio  
+fim = 0    # fim
+while i > fim:
+    print(i)
+    i -= 1 # pulo
